2e29f03cd6d871fb7f8c59610814270da879e30b
Chris Blackwell <christopherblackwell50@gmail.com>
2023-12-12 21:29:20 -0500

    Added multiply, divide

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -12,5 +12,14 @@ public class SimpleCalculator {
   }
 	
 
+	public int multiply(int a, int b) {
+        return a * b;
+    }
 	
+	 public int divide(int a, int b) {
+	        if (b == 0) {
+	            throw new IllegalArgumentException("Divider cannot be zero");
+	        }
+	        return a / b;
+	    }
 }
