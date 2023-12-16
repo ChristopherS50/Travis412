974344e0355c43e9a580267ace7908d544efd0ec
Chris Blackwell <christopherblackwell50@gmail.com>
2023-12-15 23:24:54 -0500

    other functions

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -16,9 +16,6 @@ public class SimpleCalculator {
 	    }
 		
 		 public int divide(int a, int b) {
-		        if (b == 0) {
-		            throw new IllegalArgumentException("Divider cannot be zero");
-		        }
 		        return a / b;
 		    }
 	
