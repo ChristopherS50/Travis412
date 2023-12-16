58f7c2aed06978c8bca5f817abfc730eaa551870
Chris Blackwell <christopherblackwell50@gmail.com>
2023-12-15 23:24:23 -0500

    added functions

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -3,11 +3,23 @@ package travis_ci_tutorial_java;
 public class SimpleCalculator {
 	public int add(int a, int b) {
 		
-	int a - int b;
-	
 		return a + b;
     
     }
-	
+	public int minus(int a, int b) { 
+	    return a - b;
+	  }
+		
+
+		public int multiply(int a, int b) {
+	        return a * b;
+	    }
+		
+		 public int divide(int a, int b) {
+		        if (b == 0) {
+		            throw new IllegalArgumentException("Divider cannot be zero");
+		        }
+		        return a / b;
+		    }
 	
 }
